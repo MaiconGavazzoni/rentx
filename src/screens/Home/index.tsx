6f0251aedc6e8fd7c3095943658910a7f236505c
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2021-11-01 23:26:07 -0300

    Fonts, imagens, TypesSVG

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Logo from '../../assets/logo.svg';
+
+import {
+  Container,
+  Header
+} from './styles';
+
+export function Home(){
+  return(
+    <Container>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Header>
+        <Logo 
+          width={RFValue(108)}
+          height={RFValue(12)}
+        />
+
+      </Header>
+    </Container>
+  );
+}
