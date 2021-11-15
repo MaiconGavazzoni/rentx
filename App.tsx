43811caff362a71446fb6956374a256caacac9a7
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2021-11-15 18:55:14 -0300

    Confirmação

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 //import { Home } from './src/screens/Home';
 //import { CarDetails } from './src/screens/CarDetails';
 //import { Scheduling } from './src/screens/Scheduling';
-import { SchedulingDetails } from './src/screens/SchedulingDetails';
+//import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
@@ -25,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails/>
+      <SchedulingComplete/>
     </ThemeProvider>
   );
 }
--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -3,8 +3,8 @@ import { BackButton } from '../../components/BackButton';
 import { Accessory } from '../../components/Accessory';
 import { Button } from '../../components/Button';
 import { Feather } from '@expo/vector-icons';
-import {useTheme} from 'styled-components';
-import { RFValue} from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 
 import speedSvg from '../../assets/speed.svg';
@@ -35,6 +35,11 @@ import {
   DateInfo,
   DateTitle,
   DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
   Footer
 
 } from './styles';
@@ -65,24 +70,24 @@ export function SchedulingDetails() {
 
           <Rent>
             <Period>Ao dia</Period>
-            <Price>R$ 450</Price>
+            <Price>R$ 580</Price>
           </Rent>
         </Details>
 
         <Accessories>
-          <Accessory  name={"380Km/h"} icon={speedSvg}/>
-          <Accessory  name={"3.2s"} icon={accelerationSvg}/>
-          <Accessory  name={"800 HP"} icon={forceSvg}/>
-          <Accessory  name={"Gasolina"} icon={gasolineSvg}/>
-          <Accessory  name={"Auto"} icon={exchangeSvg}/>
-          <Accessory  name={"2 pessoas"} icon={peopleSvg}/>
+          <Accessory name={"380Km/h"} icon={speedSvg} />
+          <Accessory name={"3.2s"} icon={accelerationSvg} />
+          <Accessory name={"800 HP"} icon={forceSvg} />
+          <Accessory name={"Gasolina"} icon={gasolineSvg} />
+          <Accessory name={"Auto"} icon={exchangeSvg} />
+          <Accessory name={"2 pessoas"} icon={peopleSvg} />
         </Accessories>
         <RentalPeriod>
           <CalendarIcon>
-            <Feather 
-            name='calendar'
-            size={RFValue(24)}
-            color= {theme.colors.shape}
+            <Feather
+              name='calendar'
+              size={RFValue(24)}
+              color={theme.colors.shape}
             />
           </CalendarIcon>
 
@@ -91,21 +96,30 @@ export function SchedulingDetails() {
             <DateValue>18/06/2021</DateValue>
           </DateInfo>
 
-          <Feather 
+          <Feather
             name='chevron-right'
             size={RFValue(10)}
-            color= {theme.colors.text}
-            />
+            color={theme.colors.text}
+          />
 
           <DateInfo>
-            <DateTitle>DE</DateTitle>
+            <DateTitle>ATÉ</DateTitle>
             <DateValue>18/06/2021</DateValue>
           </DateInfo>
         </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+
       </Content>
-       <Footer>
-        <Button title='Confirmar'/>
-      </Footer> 
+      <Footer>
+        <Button title='ALugar agora' color={theme.colors.success} />
+      </Footer>
     </Container>
   );
 }
